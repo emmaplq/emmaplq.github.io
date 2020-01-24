1e6467db1f37014dd9dbcea7161b55d50a2a42f3
emmaplq <55293008+emmaplq@users.noreply.github.com>
2020-01-24 13:53:19 -0500

    Update MyJavaScript.js

--- tests/MyJavaScript.js
+++ tests/MyJavaScript.js
@@ -2,3 +2,26 @@ for(var x = 0; x <= 8; x++){
   var y = Math.pow(2,x);
   document.write("number " + y + "<br />");
 }
+
+function myLoop(){ 
+var start = document.getElementById("start").value;  
+var end = document.getElementById("end").value;
+var jump = document.getElementById("jump").value;
+
+console.log(typeof start);
+console.log(typeof end);
+console.log(typeof jump);
+  
+start = Number(start);
+end = Number(end);
+jump = Number(jump);
+  
+console.log(typeof start);
+console.log(typeof end);
+console.log(typeof jump);  
+  
+for(var i = start; i <= end; i+=jump)
+  {
+    document.write('<p>' + i + '</p>');
+  }  
+  }
